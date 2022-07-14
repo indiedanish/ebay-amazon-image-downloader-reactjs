a1ff3b0a2300f20574ded84fe9d214d93c79b156
indiedanish <danishbuttdb@hotmail.com>
2022-07-14 18:21:51 +0500

    Commited

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+import {HiMenuAlt4, HiX} from 'react-icons/hi'
+import {motion} from 'framer-motion'
+
+
+import './Navbar.scss'
+
+import {images} from '../../constants'
+function Navbar() {
+
+    const [toggle, setToggle] = useState(false)
+  return (
+    <nav className="app__navbar">
+        <div className="app__navbar-logo">
+           
+            <h1>Image Downloader</h1>
+        </div>
+        <ul className="app__navbar-links">
+            {['Home','About', 'Contact','Products' ].map((item)=>(
+                <li className="app__flex p-text" key={`link-${item}`} >
+                    <div/>
+                    <a href={`#${item}`} >{item}</a>
+                  
+                </li>
+            )
+
+            ) }
+        </ul>
+
+        <div className="app__navbar-menu">
+            <HiMenuAlt4 onClick={()=>{setToggle(true)}} />
+
+            {toggle && (
+                <motion.div
+                whileInView={{
+                    x: [300,0]
+                }}
+                transition={{duration: 0.85, ease: 'easeOut' }}
+                >
+                    <HiX onClick={()=>{setToggle(false)}} />
+                 
+                    <ul className="app__navbar-links">
+                    {['Home','About', 'Contact','Products' ].map((item)=>(
+                <li  key={item} >
+                  
+                    <a onClick={()=>{setToggle(false)}} href={`#${item}`} >{item}</a>
+                  
+                </li>
+            )
+
+            ) }
+
+            </ul>l
+                </motion.div>
+
+            )}
+
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+//ss
+function Header() {
+  return (
+    <h1>
+      Header
+    </h1>
+  )
+}
+export default Header
